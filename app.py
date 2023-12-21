2ded096ee5494400d49ff7f7ff9e06d8d90eb7f8
premgitsg <33406922+premgitsg@users.noreply.github.com>
2023-12-21 16:45:18 +0000

    Added dynamics data and API Route
    Added dynamics data and API Route

--- app.py
+++ app.py
@@ -1,8 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify 
 app = Flask(__name__)
+
+JOBS = [
+  {
+    'id':1,
+    'title':'Data Analyst',
+    'location':'Bengaluru, India',
+    'salary':'RS. 10,00,000'
+  },
+  {
+    'id':2,
+    'title':'Business Analyst',
+    'location':'Bengaluru, India',
+    'salary':'RS. 10,00,000'
+  },
+  {
+  'id':3,
+  'title':'Data Engineer',
+  'location':'Chennai, India',
+  'salary': ''
+  },
+  {
+  'id':4,
+  'title':'Backend Engineer',
+  'location':'Bengaluru, India',
+  'salary':'USD. 1000'
+  }
+]
 @app.route("/")
 def hello_world():
   return render_template(
-    "home.html",)
+    "home.html", jobs=JOBS, company_name='Jovian')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+  
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080, debug=True)
